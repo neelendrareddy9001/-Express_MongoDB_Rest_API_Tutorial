99af09c48d9ad485521584a515aec3ba2c14ecc5
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-11 23:09:01 +0100

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -19,13 +19,13 @@ const registerUser = asyncHandler (async (req,res) => {
     }
 
     //Hash password
-    const hashPassword = await bcrypt.hash(password, 10);
-    console.log("Hashed Password", hashPassword);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password", hashedPassword);
 
     const user = await User.create({
         username,
         email,
-        password : hashPassword,
+        password : hashedPassword,
     });
 
     console.log(`User created ${user}`);
@@ -42,8 +42,31 @@ const registerUser = asyncHandler (async (req,res) => {
 //@Route POST /api/users/login
 //@access public
 
-const loginUser = asyncHandler (async (req,res) => {
-    res.json({message : "Login user"});
+const loginUser = asyncHandler (async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error ("All field are Mandatory");
+    }
+    const user = await User.findOne({email});
+
+    //Compare password with hashPassword
+    if(user && (await bcrypt.compare(password, user.password))) {
+
+        const accessToken = jwt.sign({
+            user : {
+                username : user.username,
+                email : user.email,
+                id : user.id,
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : "1m"}
+    );
+        res.status(200).json({accessToken});
+    } else {
+        res.status(401)
+        throw new Error ("email or password is not valid"); 
+    }
 });
 
 //@desc Current user info
