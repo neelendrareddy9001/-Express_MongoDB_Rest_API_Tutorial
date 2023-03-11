0dbff750a27339c13391a087cb92975138323c20
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-11 01:27:05 +0100

    Update contactController.js

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -12,6 +12,11 @@ const getContacts = asyncHandler(async (req, res) => {
 //@access public
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body :", req.body);
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All Field are mandatory");
+    }
     res.status(200).json({message : `Create A Contact`});
 }); 
 
