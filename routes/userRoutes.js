80c68403d03e4ba1d21b6fb7863884a48d52bcbe
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-11 18:29:44 +0100

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {registerUser, loginUser, currentUser} = require("../Controllers/userController");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post ("/login", loginUser);
+
+router.post("/current", currentUser);
+
+
+module.exports = router;
