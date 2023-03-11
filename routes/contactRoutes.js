47e0cc41428af1ea207c8067ad2d7eb8b841e3bf
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-11 18:32:50 +0100

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -7,11 +7,8 @@ const {updateContact} = require("../Controllers/contactController");
 const {deleteContact} = require("../Controllers/contactController");
 
 
-router.route("/").get(getContacts);
-
-router.route("/").post(createContact).get(getContact);
-
-router.route("/:id").put(updateContact).delete(deleteContact);
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 
 module.exports = router;
