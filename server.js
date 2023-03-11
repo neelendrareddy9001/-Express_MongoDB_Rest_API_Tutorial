fd762828dd4c0684b12fb8603f387c3a6f6222f2
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-11 23:15:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -10,6 +13,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+//Middleware Error Handler 
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`); 
